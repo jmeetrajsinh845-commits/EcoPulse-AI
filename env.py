d66dec2c3e46f1d4dfd99c76ca6c0149a117a58a
Sakshita Jadeja <jmeetrajsinh845@gmail.com>
2026-03-31 15:28:21 +0530

    env.py

--- env.py
+++ env.py
@@ -1,45 +1,31 @@
-import random
-
 class SmartEnergyEnv:
     def __init__(self):
-        self.prices = ["low"]*8 + ["high"]*10 + ["low"]*6
-        self.solar_power = ["cloudy"]*7 + ["sunny"]*11 + ["cloudy"]*6
         self.reset()
-
     def reset(self):
         self.current_hour = 0
-        self.battery = 50  # અહીં નામ 'battery' રાખવું જરૂરી છે
         self.total_reward = 0
-        return self._get_observation()
-
-    def _get_observation(self):
-        # સિસ્ટમને આ 3 કી (Keys) ખાસ જોઈએ છે
+        self.battery = 50
+        return self._get_obs()
+    def _get_obs(self):
+        prices = ['low', 'medium', 'high']
+        weather = ['sunny', 'cloudy']
+        # અહીં બેટરીને સીધી સંખ્યા (int) તરીકે જ મોકલીએ
         return {
-            "grid_price": self.prices[self.current_hour],
-            "solar_ready": self.solar_power[self.current_hour],
-            "battery": self.battery 
+            'grid_price': prices[self.current_hour % 3],
+            'solar_ready': weather[0] if self.current_hour < 15 else weather[1],
+            'battery': self.battery 
         }
-
     def step(self, action):
-        price = self.prices[self.current_hour]
-        sun = self.solar_power[self.current_hour]
         reward = 0
-        
-        if action == 0: # Solar
-            reward += 25 if sun == "sunny" else -15
-        elif action == 1: # Discharge Battery
-            self.battery -= 10
-            reward += 20 if price == "high" else 5
-        elif action == 2: # Grid
-            reward -= 25 if price == "high" else 5
-        elif action == 3: # Charge Battery
-            self.battery += 15
-            reward += 15 if price == "low" else -15
-            
-        # બેટરીની મર્યાદા નક્કી કરવી (0 થી 100 ની વચ્ચે)
-        self.battery = max(0, min(100, self.battery))
-        
-        self.current_hour = (self.current_hour + 1) % 24
-        done = (self.current_hour == 0)
-        
-        return self._get_observation(), reward, done
+        if action == 0: reward = 10 
+        elif action == 1: 
+            if self.battery >= 10: self.battery -= 10; reward = 5
+            else: reward = -5
+        elif action == 2: reward = -2 
+        elif action == 3: 
+            if self.battery <= 90: self.battery += 10; reward = 2
+            else: reward = -1
+        self.current_hour += 1
+        done = self.current_hour >= 24
+        self.total_reward += reward
+        return self._get_obs(), reward, done
